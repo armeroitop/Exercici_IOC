5a9270653dd51f3fb7cf0edf5ee7c798eabc407a
David Gerardo <armeroitop@gmail.com>
2023-09-11 17:32:15 +0200

    agregado comentarios javadoc

--- src/pruebaGit/ClasePrincipal.java
+++ src/pruebaGit/ClasePrincipal.java
@@ -1,7 +1,11 @@
 package pruebaGit;
 
 public class ClasePrincipal {
-
+	
+	/**
+	 *Exemple de descripció general d'un comentari <u>JavaDoc.</u>
+	  @ Author Programador primer del projecte
+	 */
 	public static void main(String[] args) {
 
 
